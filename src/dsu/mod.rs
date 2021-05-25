a080f9d20d889d8b8042fe103d5252381110d0aa
myduomilia <myduomilia@gmail.com>
2021-05-25 23:27:19 +0400

    DSU - added ranks

--- src/dsu/mod.rs
+++ src/dsu/mod.rs
@@ -31,18 +31,21 @@ use std::collections::BTreeMap;
 /// ```
 
 pub struct DSURef<'a, T> where T: Eq + Ord {
-    parent: BTreeMap<&'a T, &'a T>
+    parent: BTreeMap<&'a T, &'a T>,
+    ranks: BTreeMap<&'a T, usize>
 }
 
 impl <'a, T> DSURef<'a, T> where T: Eq + Ord {
     pub fn new() -> Self {
         DSURef {
             parent: BTreeMap::new(),
+            ranks: BTreeMap::new()
         }
     }
     pub fn make_set(&mut self, value: &'a T) {
         if !self.parent.contains_key(&value) {
             self.parent.insert(value, value);
+            self.ranks.insert(value, 1);
         }
     }
 
@@ -64,25 +67,38 @@ impl <'a, T> DSURef<'a, T> where T: Eq + Ord {
         let second = self.find_set(second);
         if first.is_some() && second.is_some() {
             if *first.unwrap() != *second.unwrap() {
-                *self.parent.get_mut(second.unwrap()).unwrap() = first.unwrap();
+                let first_rank = *self.ranks.get(first.as_ref().unwrap()).unwrap();
+                let second_rank = *self.ranks.get(second.as_ref().unwrap()).unwrap();
+                if second_rank >= first_rank {
+                    let key = second.unwrap();
+                    *self.parent.get_mut(&key).unwrap() = first.unwrap();
+                    *self.ranks.get_mut(&key).unwrap() = first_rank + second_rank;
+                } else {
+                    let key = first.unwrap();
+                    *self.parent.get_mut(&key).unwrap() = second.unwrap();
+                    *self.ranks.get_mut(&key).unwrap() = first_rank + second_rank;
+                }
             }
         }
     }
 }
 
 pub struct DSU<T> where T: Eq + Ord + Clone {
-    parent: BTreeMap<T, T>
+    parent: BTreeMap<T, T>,
+    ranks: BTreeMap<T, usize>
 }
 
 impl <T> DSU<T> where T: Eq + Ord + Clone {
     pub fn new() -> Self {
         DSU {
             parent: BTreeMap::new(),
+            ranks: BTreeMap::new()
         }
     }
     pub fn make_set(&mut self, value: T) {
         if !self.parent.contains_key(&value) {
-            self.parent.insert(value.clone(), value);
+            self.parent.insert(value.clone(), value.clone());
+            self.ranks.insert(value.clone(), 1);
         }
     }
 
@@ -104,14 +120,24 @@ impl <T> DSU<T> where T: Eq + Ord + Clone {
         let second = self.find_set(second);
         if first.is_some() && second.is_some() {
             if first.as_ref().unwrap() != second.as_ref().unwrap() {
-                *self.parent.get_mut(&second.unwrap()).unwrap() = first.unwrap();
+                let first_rank = *self.ranks.get(&first.as_ref().unwrap()).unwrap();
+                let second_rank = *self.ranks.get(&second.as_ref().unwrap()).unwrap();
+                if second_rank >= first_rank {
+                    let key = second.unwrap();
+                    *self.parent.get_mut(&key).unwrap() = first.unwrap();
+                    *self.ranks.get_mut(&key).unwrap() = first_rank + second_rank;
+                } else {
+                    let key = first.unwrap();
+                    *self.parent.get_mut(&key).unwrap() = second.unwrap();
+                    *self.ranks.get_mut(&key).unwrap() = first_rank + second_rank;
+                }
             }
         }
     }
 }
 
 #[test]
-fn test_dsu() {
+fn test_dsu_ref() {
     let mut dsu = DSURef::new();
     let v = (0..10).collect::<Vec<u32>>();
     for i in &v {
@@ -129,7 +155,7 @@ fn test_dsu() {
 }
 
 #[test]
-fn test_dsu_cloneable() {
+fn test_dsu() {
     let mut dsu = DSU::new();
     for i in 0..10 {
         dsu.make_set(i);
