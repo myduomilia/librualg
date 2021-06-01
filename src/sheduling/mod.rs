5bb4912eb46e7812840c8236abdba38f3997dc97
myduomilia <myduomilia@gmail.com>
2021-06-01 11:17:40 +0400

    Johnson's Algorithm For Scheduling

--- src/sheduling/mod.rs
+++ src/sheduling/mod.rs
@@ -1,10 +1,31 @@
 use std::cmp::{Ordering, min, max};
 
+#[derive(Copy, Clone, Debug)]
+pub struct Job {
+    id: usize,
+    first_time: u32,
+    second_time: u32
+}
+
+impl Job {
+    pub fn new(id: usize, first_time: u32, second_time: u32) -> Self {
+        Job{
+            id,
+            first_time,
+            second_time
+        }
+    }
+    pub fn get_id(&self) -> usize {
+        self.id
+    }
+}
+
 /// Johnson's Algorithm For Scheduling
 /// There are n parts and two machines. Each part must first be machined on the first machine, then on the second one.
 /// The i-th part is machined on the first machine in a_i time, and on the second machine in b_i time.
 /// Each machine can handle only one part at any given time.
 /// You need to create an order of feeding parts to the machines, so that the total machining time of all parts would be minimal.
+/// return (order_jobs, total time)
 ///```
 /// use librualg::sheduling::{johnson_algorithm, Job};
 ///
@@ -17,30 +38,9 @@ use std::cmp::{Ordering, min, max};
 /// ];
 ///
 /// let (jobs, total_time) = johnson_algorithm(&jobs);
-/// assert_eq!(vec![5, 1, 4, 3, 2], jobs.iter().map(|value| value.get_id()).collect::<Vec<u32>>());
+/// assert_eq!(vec![5, 1, 4, 3, 2], jobs.iter().map(|value| value.get_id()).collect::<Vec<usize>>());
 /// assert_eq!(47, total_time);
 /// ```
-
-#[derive(Copy, Clone, Debug)]
-pub struct Job {
-    id: u32,
-    first_time: u32,
-    second_time: u32
-}
-
-impl Job {
-    pub fn new(id: u32, first_time: u32, second_time: u32) -> Self {
-        Job{
-            id,
-            first_time,
-            second_time
-        }
-    }
-    pub fn get_id(&self) -> u32 {
-        self.id
-    }
-}
-
 pub fn johnson_algorithm(jobs: &[Job]) -> (Vec<Job>, u32) {
     let mut res = jobs.to_vec();
     let mut total = 0;
@@ -70,6 +70,6 @@ fn test(){
     ];
 
     let (jobs, total_time) = johnson_algorithm(&jobs);
-    assert_eq!(vec![5, 1, 4, 3, 2], jobs.iter().map(|value| value.id).collect::<Vec<u32>>());
+    assert_eq!(vec![5, 1, 4, 3, 2], jobs.iter().map(|value| value.id).collect::<Vec<usize>>());
     assert_eq!(47, total_time);
 }
--- tests/sheduling.rs
+++ tests/sheduling.rs
@@ -11,6 +11,6 @@ fn test(){
     ];
 
     let (jobs, total_time) = johnson_algorithm(&jobs);
-    assert_eq!(vec![5, 1, 4, 3, 2], jobs.iter().map(|value| value.get_id()).collect::<Vec<u32>>());
+    assert_eq!(vec![5, 1, 4, 3, 2], jobs.iter().map(|value| value.get_id()).collect::<Vec<usize>>());
     assert_eq!(47, total_time);
 }
