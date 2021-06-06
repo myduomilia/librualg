a4f83804818eae432e15c05471474ee60d754eeb
myduomilia <myduomilia@gmail.com>
2021-06-06 15:22:21 +0400

    fix test of next permutation

--- src/combinatorics/mod.rs
+++ src/combinatorics/mod.rs
@@ -7,7 +7,7 @@ use std::option::Option::Some;
 /// let arr = vec![[0, 2, 1], [1, 0, 2], [1, 2, 0], [2, 0, 1], [2, 1, 0]];
 /// let mut values = vec![0, 1, 2];
 /// let mut idx = 0;
-/// while let Some(_) = next_permutation(&mut v) {
+/// while let Some(_) = next_permutation(&mut values) {
 ///     assert_eq!(vec![values[0], values[1], values[2]], arr[idx]);
 ///     idx += 1;
 /// }
