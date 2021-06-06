d1c3d63ece048e84a383ad826cddc1ef4e8cc380
myduomilia <myduomilia@gmail.com>
2021-06-06 15:21:24 +0400

    fix test of next permutation

--- tests/combinatorics.rs
+++ tests/combinatorics.rs
@@ -1,12 +1,15 @@
 extern crate librualg;
 
-use librualg::combinatorics::Permutations;
+use librualg::combinatorics::next_permutation;
 
 #[test]
 fn permutations(){
-    let per = Permutations::new(3);
-    let arr = vec![[0, 1, 2], [0, 2, 1], [1, 0, 2], [1, 2, 0], [2, 0, 1], [2, 1, 0]];
-    for (i, value) in per.into_iter().enumerate() {
-        assert_eq!(vec![value[0], value[1], value[2]], arr[i]);
+    let arr = vec![[0, 2, 1], [1, 0, 2], [1, 2, 0], [2, 0, 1], [2, 1, 0]];
+    let mut values = vec![0, 1, 2];
+    let mut idx = 0;
+    while let Some(_) = next_permutation(&mut values) {
+        assert_eq!(vec![values[0], values[1], values[2]], arr[idx]);
+        idx += 1;
     }
+    assert_eq!(idx, arr.len());
 }
